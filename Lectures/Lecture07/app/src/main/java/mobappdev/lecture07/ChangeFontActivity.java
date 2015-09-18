c4f602f9b1934bb00f32ab461abf67bf7bb917f0
rstjacqu <Robert.StJacques@parc.com>
2015-09-18 13:17:31 -0400

    Lecture07 Initial Commit.

---
+++ Lectures/Lecture07/app/src/main/java/mobappdev/lecture07/ChangeFontActivity.java
@@ -0,0 +1,16 @@
+package mobappdev.lecture07;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChangeFontActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_font);
+    }
+
+}
