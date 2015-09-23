048aadbc2459f19d5a62417a76ab2944861f979c
rstjacqu <robert.stjacques@xerox.com>
2015-09-23 10:03:22 -0400

    Commit initial working app.  Contains one layout fragment (hard wired) and two dynamic fragments that are switched at runtime using the FragmentManager in response to clicking buttons in the UI.
    Also has examples of XML onClick listeners.

--- Lectures/Lecture08/app/src/main/java/mobappdev/lecture08/FragmentActivity.java
+++ Lectures/Lecture08/app/src/main/java/mobappdev/lecture08/FragmentActivity.java
@@ -1,37 +1,49 @@
 package mobappdev.lecture08;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class FragmentActivity extends Activity {
+
+    private static final String TAG = "FragmentActivity";
+
+    private Fragment mFirstFragment;
+    private Fragment mSecondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        mFirstFragment = new FirstFragment();
+        mSecondFragment = new SecondFragment();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void firstFragment(View view) {
+        Log.d(TAG, "firstFragment(View) called");
+        if(mFirstFragment.isAdded() == false) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .remove(mSecondFragment) // safe to remove even if it's not added
+                    .add(R.id.frame_layout_fragments, mFirstFragment)
+                    .commit();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void secondFragment(View view) {
+        Log.d(TAG, "secondFragment(View) called");
+        if(mSecondFragment.isAdded() == false) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .remove(mFirstFragment) // safe to remove even if it's not added
+                    .add(R.id.frame_layout_fragments, mSecondFragment)
+                    .commit();
+        }
     }
 }
--- Lectures/Lecture08/app/src/main/java/mobappdev/lecture08/SecondFragment.java
+++ Lectures/Lecture08/app/src/main/java/mobappdev/lecture08/SecondFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ExampleFragment extends Fragment {
+public class SecondFragment extends Fragment {
 
 
-    public ExampleFragment() {
+    public SecondFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,8 @@ public class ExampleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_example, container, false);
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
+
 
 }
