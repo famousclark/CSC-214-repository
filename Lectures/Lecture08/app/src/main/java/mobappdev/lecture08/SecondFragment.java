2a0ee82075ca0cf0bd3470449c1be1779b733143
rstjacqu <robert.stjacques@xerox.com>
2015-09-23 11:58:05 -0400

    Added an example of building a color from int values.

--- Lectures/Lecture08/app/src/main/java/mobappdev/lecture08/SecondFragment.java
+++ Lectures/Lecture08/app/src/main/java/mobappdev/lecture08/SecondFragment.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +24,20 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View content = inflater.inflate(R.layout.fragment_second, container, false);
+
+        TextView color = (TextView)content.findViewById(R.id.text_view_color);
+
+        int alpha = 128; // about 50% transparent
+        int red = 255; // fully red
+        int green = 255; // fully green
+        int blue = 0; // no blue
+
+        //shift the alpha 3 bytes to the left, the red 2 bytes, the green 1 byte
+        int paleYellow = (alpha << 24 ) + (red << 16) + (green << 8) + blue;
+        color.setBackgroundColor(paleYellow);
+
+        return content;
     }
 
 
