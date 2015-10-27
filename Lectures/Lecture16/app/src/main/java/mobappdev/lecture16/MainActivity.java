b633e5e94fab26e04ec409121aef611a858de7cb
rstjacqu <Robert.StJacques@parc.com>
2015-10-27 17:40:35 -0400

    Implemented using an implicit intent to start a browser.

--- Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/MainActivity.java
+++ Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/MainActivity.java
@@ -1,13 +1,29 @@
 package mobappdev.lecture16;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUrl = (EditText)findViewById(R.id.edit_text_url);
+    }
+
+    public void browseToUrl(View view) {
+        String uriString = mUrl.getText().toString();
+        Uri location = Uri.parse(uriString);
+        Intent intent = new Intent(Intent.ACTION_VIEW, location);
+        intent.setType("text/html");
+        startActivity(intent);
     }
 }
