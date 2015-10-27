bfe56bf8abf79d97e45e5a8b2203759f1e6ccb38
rstjacqu <Robert.StJacques@parc.com>
2015-10-27 18:35:05 -0400

    Separated the activity that opens the browser using an implicit intent into a second activity because this example will eventually need to launch multiple sub-activities that do different things.

---
+++ Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/BrowseToActivity.java
@@ -0,0 +1,37 @@
+package mobappdev.lecture16;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class BrowseToActivity extends AppCompatActivity {
+
+    private EditText mUrl;
+
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, BrowseToActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_to);
+        mUrl = (EditText)findViewById(R.id.edit_text_url);
+    }
+
+    public void browseToUrl(View view) {
+        String uriString = mUrl.getText().toString();
+        if(uriString.startsWith("http") == false) {
+            uriString = "http://" + uriString;
+        }
+        Uri location = Uri.parse(uriString);
+        Intent intent = new Intent(Intent.ACTION_VIEW, location);
+        intent.setType("text/html");
+        startActivity(intent);
+    }
+}
