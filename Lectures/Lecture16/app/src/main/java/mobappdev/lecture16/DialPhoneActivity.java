bb6b836c3262e30cf7c32d744fd4489504eaf7a4
rstjacqu <Robert.StJacques@parc.com>
2015-10-28 15:13:44 -0400

    Implementing phone dialing.

--- Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/DialPhoneActivity.java
+++ Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/DialPhoneActivity.java
@@ -2,11 +2,15 @@ package mobappdev.lecture16;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class DialPhoneActivity extends AppCompatActivity {
+
+    private EditText mNumber;
 
     public static Intent newIntent(Context context) {
         Intent intent = new Intent(context, DialPhoneActivity.class);
@@ -17,12 +21,15 @@ public class DialPhoneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial_phone);
+
+        mNumber = (EditText)findViewById(R.id.edit_text_number);
     }
 
     public void dialThePhone(View view) {
         Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_DIAL);
-
+        intent.setAction(Intent.ACTION_CALL);
+        Uri number = Uri.parse("tel:" + mNumber.getText().toString());
+        intent.setData(number);
         startActivity(intent);
     }
 }
