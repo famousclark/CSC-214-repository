9453402f09efa346a822621046609bd8d7e0ac48
rstjacqu <Robert.StJacques@parc.com>
2015-10-28 14:55:38 -0400

    Made some changes, then reverted back.

--- Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/ContactsActivity.java
+++ Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/ContactsActivity.java
@@ -2,14 +2,17 @@ package mobappdev.lecture16;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.ContactsContract.Contacts;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ContactsActivity extends AppCompatActivity {
+
+    private TextView mContactName;
 
     public static Intent newIntent(Context context) {
         Intent intent = new Intent(context, ContactsActivity.class);
@@ -21,6 +24,35 @@ public class ContactsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
+        mContactName = (TextView)findViewById(R.id.text_view_contact_name);
     }
 
+    public void pickAContact(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_PICK);
+        intent.setData(Contacts.CONTENT_URI);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(data != null) {
+            Uri contactUri = data.getData();
+            String queryFields[] = new String[] {
+                    Contacts.DISPLAY_NAME,
+            };
+            Cursor cursor = getContentResolver().query(contactUri, queryFields, null, null, null);
+            try {
+                if(cursor.getCount() != 0) {
+                    cursor.moveToFirst();
+                    String contactName = cursor.getString(
+                            cursor.getColumnIndex(Contacts.DISPLAY_NAME));
+                    mContactName.setText(contactName);
+                }
+            }
+            finally {
+                cursor.close();
+            }
+        }
+    }
 }
--- Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/DialPhoneActivity.java
+++ Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/DialPhoneActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DialPhoneActivity extends AppCompatActivity {
 
@@ -16,5 +17,12 @@ public class DialPhoneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial_phone);
+    }
+
+    public void dialThePhone(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_DIAL);
+
+        startActivity(intent);
     }
 }
