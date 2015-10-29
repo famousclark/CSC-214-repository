302de8bf0f321f28a396d3b9585ad0b4aeca8ea1
bobby <rstjacqu@cs.rochester.edu>
2015-10-29 00:46:13 -0400

    Taking photos now works.

--- Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/CameraActivity.java
+++ Lectures/Lecture16/app/src/main/java/mobappdev/lecture16/CameraActivity.java
@@ -3,17 +3,27 @@ package mobappdev.lecture16;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.util.UUID;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private static final String TAG = "CameraActivity";
     private ImageView mPhoto;
+    private File mPhotoFile;
 
     public static Intent newIntent(Context context) {
         Intent intent = new Intent(context, CameraActivity.class);
@@ -24,20 +34,83 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        mPhoto = (ImageView)findViewById(R.id.image_view_photo);
+        mPhoto = (ImageView)findViewById(R.id.image_button_photo);
     }
 
     public void takeAPhoto(View view) {
         Intent intent = new Intent();
         intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        //make a random filename
+        String filename = "IMG_" + UUID.randomUUID().toString() + ".jpg";
+        //make a file in the external photos directory
+        File picturesDir =
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        mPhotoFile = new File(picturesDir, filename);
+
+        Uri photoUri = Uri.fromFile(mPhotoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+        Log.d(TAG, "photo location: " + mPhotoFile.toString());
+
         startActivityForResult(intent, 0);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult()");
         if(resultCode == Activity.RESULT_OK) {
-
+            updatePhoto();
         }
+    }
+
+    private void updatePhoto() {
+        if(mPhotoFile == null || mPhotoFile.exists() == false) {
+            Log.d(TAG, "Photo doesn't exist at: " + mPhotoFile.getPath());
+            mPhoto.setImageDrawable(null);
+        }
+        else {
+            Log.d(TAG, "Photo exists!");
+            Bitmap photo = getScaledBitmap(mPhotoFile.getPath(),
+                    mPhoto.getWidth(), mPhoto.getHeight());
+            mPhoto.setImageBitmap(photo);
+        }
+    }
+
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay().getSize(size);
+
+        return getScaledBitmap(path, size.x, size.y);
+    }
+
+    public static Bitmap getScaledBitmap(String path, int width, int height) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        Log.d(TAG, "width=" + width + "," + "height=" + height);
+        Log.d(TAG, "srcWidth=" + srcWidth + "," + "srcHeight=" + srcHeight);
+
+        int sampleSize = 1;
+        if(srcHeight > height || srcWidth > width ) {
+            if(srcWidth > srcHeight) {
+                sampleSize = Math.round(srcHeight / height);
+            }
+            else {
+                sampleSize = Math.round(srcWidth / width);
+            }
+        }
+
+        Log.d(TAG, "sampleSize=" + sampleSize);
+
+        BitmapFactory.Options scaledOptions = new BitmapFactory.Options();
+        scaledOptions.inSampleSize = sampleSize;
+
+        return BitmapFactory.decodeFile(path, scaledOptions);
+
     }
 }
